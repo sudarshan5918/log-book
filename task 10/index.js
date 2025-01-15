6ae516be0ab54769a422f83403dc682f0d12f4e3
sudarshan5918 <maxman5918@gmail.com>
2025-01-15 14:48:14 +0530

    Add files via upload

---
+++ task 10/index.js
@@ -0,0 +1,18 @@
+let score = prompt("enter your score?");
+if
+    (score >= 90) {
+    alert("Grade: A");
+
+} else if (score >= 75) {
+    alert("Grade: B");
+} else {
+    alert("Grade:c");
+}
+
+let num =prompt("check odd and even");
+if (num%2==0){
+    alert("the number is even");
+
+}else if (num%2!==0){
+    alert("the number is odd")
+}
